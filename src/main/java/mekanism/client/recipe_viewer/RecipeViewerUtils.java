0d73cc52e228ce706c501ae63ec343821dd05671
Sara Freimer <sara@freimer.com>
2026-05-11 11:42:24 -0500

    Remove no longer needed legacy insert/extract methods on fluid tanks and work towards doing the same for chemical tanks

--- src/main/java/mekanism/client/recipe_viewer/RecipeViewerUtils.java
+++ src/main/java/mekanism/client/recipe_viewer/RecipeViewerUtils.java
@@ -26,7 +26,7 @@ import mekanism.common.recipe.IMekanismRecipeTypeProvider;
 import mekanism.common.recipe.MekanismRecipeType;
 import mekanism.common.tier.ChemicalTankTier;
 import mekanism.common.tile.machine.TileEntityNutritionalLiquifier;
-import mekanism.common.util.ChemicalUtil;
+import mekanism.common.util.ChemicalUtils;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.Holder;
@@ -134,7 +134,7 @@ public class RecipeViewerUtils {
         List<ItemStack> stacks = new ArrayList<>();
         //Always include the chemical tank of the type to portray that we accept items
         for (Holder<Chemical> type : supportedTypes) {
-            stacks.add(ChemicalUtil.getFullChemicalTank(ChemicalTankTier.BASIC, type));
+            stacks.add(ChemicalUtils.getFullChemicalTank(ChemicalTankTier.BASIC, type));
         }
         ContextMap slotDisplayContext = getSlotDisplayContext();
         //See if there are any chemical to item mappings
